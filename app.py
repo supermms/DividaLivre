933de51fee10c4c198d277f06576e76958932ab7
Matheus Moreno Sá <mms.matheus1@gmail.com>
2024-07-15 21:08:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,125 +8,57 @@ app = Flask(__name__)
 
 # Load .env file
 load_dotenv("./.env")
-messenger = WhatsApp(os.getenv("TOKEN"),
-                     phone_number_id=os.getenv("ID"))
+
+# Access token for your WhatsApp business account app
+whatsapp_token = os.getenv("WHATSAPP_TOKEN")
+
+# Verify Token defined when configuring the webhook
 VERIFY_TOKEN = "batmanbatman"
+
+messenger = WhatsApp(os.getenv("TOKEN"),
+                     phone_number_id=os.getenv("PHONE_NUMBER_ID"))
+
+
+# Required webhook verifictaion for WhatsApp
+# info on verification request payload:
+# https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+def verify(request):
+    # Parse params from the webhook verification request
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+    # Check if a token and mode were sent
+    if mode and token:
+        # Check the mode and token sent are correct
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            # Respond with 200 OK and challenge token from the request
+            print("WEBHOOK_VERIFIED")
+            return challenge, 200
+        else:
+            # Responds with '403 Forbidden' if verify tokens do not match
+            print("VERIFICATION_FAILED")
+            return jsonify({"status": "error", "message": "Verification failed"}), 403
+    else:
+        # Responds with '400 Bad Request' if verify tokens do not match
+        print("MISSING_PARAMETER")
+        return jsonify({"status": "error", "message": "Missing parameters"}), 400
+
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-@app.route('/webhook', methods=['GET'])
-def verify_token():
-    if request.args.get("hub.verify_token") == VERIFY_TOKEN:
-        logging.info("Verified webhook")
-        challenge = request.args.get("hub.challenge")
-        return str(challenge)
-    logging.error("Webhook Verification failed")
-    return "Invalid verification token"
-
-@app.route('/webhook', methods=['POST'])
-@app.post("/")
-def hook():
-    # Handle Webhook Subscriptions
-    data = request.get_json()
-    if data is None:
-        return Response(status=200)
-    logging.info("Received webhook data: %s", data)
-    changed_field = messenger.changed_field(data)
-    if changed_field == "messages":
-        new_message = messenger.is_message(data)
-        if new_message:
-            msg = Message(instance=messenger, data=data)
-            mobile = msg.sender
-            name = msg.name
-            message_type = msg.type
-            logging.info(
-                f"New Message; sender:{mobile} name:{name} type:{message_type}"
-            )
-            if message_type == "text":
-                message = msg.content
-                name = msg.name
-                logging.info("Message: %s", message)
-                m = Message(instance=messenger, to=mobile,
-                            content="Hello World")
-                m.send()
-
-            elif message_type == "interactive":
-                message_response = msg.interactive
-                if message_response is None:
-                    return Response(status=400)
-                interactive_type = message_response.get("type")
-                message_id = message_response[interactive_type]["id"]
-                message_text = message_response[interactive_type]["title"]
-                logging.info(
-                    f"Interactive Message; {message_id}: {message_text}")
-
-            elif message_type == "location":
-                message_location = msg.location
-                if message_location is None:
-                    return Response(status=400)
-                message_latitude = message_location["latitude"]
-                message_longitude = message_location["longitude"]
-                logging.info("Location: %s, %s",
-                             message_latitude, message_longitude)
-
-            elif message_type == "image":
-                image = msg.image
-                if image is None:
-                    return Response(status=400)
-                image_id, mime_type = image["id"], image["mime_type"]
-                image_url = messenger.query_media_url(image_id)
-                if image_url is None:
-                    return Response(status=400)
-                image_filename = messenger.download_media(image_url, mime_type)
-                logging.info(f"{mobile} sent image {image_filename}")
-
-            elif message_type == "video":
-                video = msg.video
-                if video is None:
-                    return Response(status=400)
-                video_id, mime_type = video["id"], video["mime_type"]
-                video_url = messenger.query_media_url(video_id)
-                if video_url is None:
-                    return Response(status=400)
-                video_filename = messenger.download_media(video_url, mime_type)
-                logging.info(f"{mobile} sent video {video_filename}")
-
-            elif message_type == "audio":
-                audio = msg.audio
-                if audio is None:
-                    return Response(status=400)
-                audio_id, mime_type = audio["id"], audio["mime_type"]
-                audio_url = messenger.query_media_url(audio_id)
-                if audio_url is None:
-                    return Response(status=400)
-                audio_filename = messenger.download_media(audio_url, mime_type)
-                logging.info(f"{mobile} sent audio {audio_filename}")
-
-            elif message_type == "document":
-                file = msg.document
-                if file is None:
-                    return Response(status=400)
-                file_id, mime_type = file["id"], file["mime_type"]
-                file_url = messenger.query_media_url(file_id)
-                if file_url is None:
-                    return Response(status=400)
-                file_filename = messenger.download_media(file_url, mime_type)
-                logging.info(f"{mobile} sent file {file_filename}")
-            else:
-                logging.info(f"{mobile} sent {message_type} ")
-                logging.info(data)
-        else:
-            delivery = messenger.get_delivery(data)
-            if delivery:
-                logging.info(f"Message : {delivery}")
-            else:
-                logging.info("No new message")
-    return "OK", 200
+# Accepts POST and GET requests at /webhook endpoint
+@app.route("/webhook", methods=["POST", "GET"])
+def webhook():
+    if request.method == "GET":
+        return verify(request)
+    elif request.method == "POST":
+        return handle_message(request)
 
 
-app.route('/peido', methods=['GET'])
+@app.route('/peido', methods=['GET'])
     logging.info('peido')
 
 if __name__ == "__main__":
